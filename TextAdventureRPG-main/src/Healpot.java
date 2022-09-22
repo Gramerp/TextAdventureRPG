311aac2b9a5cc115f83ae092b14bba8cfcae3b73
Grammerp <101070859+Gramerp@users.noreply.github.com>
2022-09-21 20:01:23 -0700

    5.9.2

---
+++ TextAdventureRPG-main/src/Healpot.java
@@ -0,0 +1,14 @@
+import java.util.Random;
+
+public class Healpot extends Consumable {
+    int healbonus;
+    public static String[] nameList = {"Life Elixir", "Ointment", "Medicine"};
+    Random rnd = new Random(System.currentTimeMillis()+Room.clocker);
+
+    public Healpot()
+    {
+        this.healbonus = rnd.nextInt(5)+1;
+        this.name = nameList[rnd.nextInt(nameList.length)]+" "+this.healbonus;
+        this.iString = "There is a "+this.name+" here.";
+    }
+}
